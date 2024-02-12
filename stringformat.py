220465e994d8f4b82877955b5c486e1995be0499
bl4ckh0st <bl4ckh0st@proton.me>
2024-02-11 22:17:44 -0600

    ejemplo basico del stringformat

---
+++ stringformat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+name = "admin"
+rol = "programador"
+edad = 35
+# Maneras de presentar informacion
+print("Hola soy %s y soy %s y tengo %d anos" % (name, rol, edad))
+print("Hola soy {0} y soy {1} y tengo {2} anos".format(name, rol, edad))
+print(f"Hola soy {name} y soy {rol} y tengo {edad} anos")
