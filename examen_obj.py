aa6c7c8f4fb09b53bf7f6e9d58559736a7911488
bl4ckh0st <n0b0dy@localhost.localdomain>
2024-02-19 23:25:15 -0600

    Avance del examen sobre objetos, metodos e instancias

---
+++ examen_obj.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Ejercicio sobre self y Objetos
+
+# Hacer un programa que pida al usuario el nombre y la edad
+# y trabajar con Objetos y todo lo que se ha visto en python3
+
+nombre = input("Introduzca el nombre porfavor: ")
+edad_str = input ("Introduzca la edad: ")
+edad = int(edad_str)
+
+class Persona(): #
+    def __init__(self, nombre, edad): #  Persona.__init__(persona.nombre persona.edad)
+        self.nombre = nombre # persona.nombre
+        self.edad = edad  #  persona.edad
+
+    def saludo(self):
+        print (f"Su nombre es {self.nombre} y tiene {self.edad} primaveras")
+persona = Persona(nombre, edad)
+persona.saludo() # llamamos a una instancia llamada persona
